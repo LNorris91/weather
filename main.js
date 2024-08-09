89591e08a57aa5dbe74330995c7d5ca57be1f8b9
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-08-09 17:11:40 -0400

    ADDS ALL THE OTHER FUNCTIONS
    this commit adds all the functions needed to make
    the app work.

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const input = document.getElementById('input');
 const searchBtn = document.getElementById('searchBtn');
+const unitBtn = document.getElementById('unitBtn');
+const locationName = document.getElementById('name');
+const locationCondition = document.getElementById('condition');
+const locationTemp = document.getElementById('temp');
+const locationFeelsLike = document.getElementById('feelsLike');
+const locationDescription = document.getElementById('description');
+const locationUv = document.getElementById('uv');
+const img = document.querySelector('img');
 
 async function getWeather(location) {
   try {
@@ -7,22 +15,99 @@ async function getWeather(location) {
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=3RUT7DGF8DMF3DT52R42KBN4J`,
       { mode: 'cors' }
     );
-    const weatherData = await response.json();
-    results = {
-      condition: weatherData.currentConditions.conditions,
-      description: weatherData.description,
-      feelslike: weatherData.currentConditions.feelslike,
-      name: weatherData.resolvedAddress,
-      uv: weatherData.currentConditions.uvindex,
-      temp: weatherData.currentConditions.temp,
-    };
-    console.log(weatherData);
+    if (response.ok) {
+      const weatherData = await response.json();
+      results = {
+        condition: weatherData.currentConditions.conditions,
+        description: weatherData.description,
+        feelslike: weatherData.currentConditions.feelslike,
+        name: weatherData.resolvedAddress,
+        sunrise: weatherData.currentConditions.sunrise,
+        sunset: weatherData.currentConditions.sunset,
+        temp: weatherData.currentConditions.temp,
+        uv: weatherData.currentConditions.uvindex,
+      };
+      console.log(results);
+      getGif(results.condition);
+      updateInfo();
+    } else {
+      if (response.status === 400) alert('Not a valid location');
+    }
   } catch (err) {
+    console.log(err.status);
     alert(err);
   }
-  console.log(results);
+  return results;
 }
 
-searchBtn.addEventListener('click', () => {
-  getWeather(input.value);
-});
+function validateForm() {
+  if (!input.value) {
+    alert('please enter a location');
+  } else {
+    getWeather(input.value);
+    input.value = '';
+  }
+}
+
+function updateInfo() {
+  locationName.textContent = results.name;
+  locationCondition.textContent = results.condition;
+  locationTemp.textContent = results.temp + '°';
+
+  if (results.feelslike !== results.temp) {
+    locationFeelsLike.textContent = 'feels like ' + results.feelslike + '°';
+  } else {
+    locationFeelsLike.textContent = 'feels exactly like it says';
+  }
+
+  locationDescription.textContent = results.description;
+
+  if (results.uv < 3) {
+    locationUv.textContent = 'UV index is ' + results.uv + ', no need to bother with sunscreen';
+  } else if (2 < results.uv && results.uv < 8) {
+    locationUv.textContent = 'UV index is ' + results.uv + ", sunscreen wouldn't hurt";
+  } else if (results.uv > 7) {
+    locationUv.textContent = 'UV index is ' + results.uv + ', lather it on THICK';
+  }
+}
+
+async function getGif(search) {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=c8c0GTCiq0bIrcW667RjAoS6zwYSAXJY&s=${search}`,
+      { mode: 'cors' }
+    );
+    const gifData = await response.json();
+    img.src = gifData.data.images.original.url;
+  } catch (err) {
+    alert(err);
+    img.src = 'icons8-emoji-64.png';
+  }
+}
+
+function toggleUnits() {
+  if (unitBtn.textContent === '°F') {
+    results.temp = convertToC(results.temp).toFixed(1);
+    results.feelslike = convertToC(results.feelslike).toFixed(1);
+    updateInfo();
+    unitBtn.textContent = '°C';
+  } else if (unitBtn.textContent === '°C') {
+    results.temp = convertToF(results.temp).toFixed(1);
+    results.feelslike = convertToF(results.feelslike).toFixed(1);
+    updateInfo();
+    unitBtn.textContent = '°F';
+  }
+}
+
+function convertToC(temp) {
+  return ((temp - 32) * 5) / 9;
+}
+
+function convertToF(temp) {
+  return (temp * 9) / 5 + 32;
+}
+
+searchBtn.addEventListener('click', validateForm);
+unitBtn.addEventListener('click', toggleUnits);
+
+getWeather('mooloolaba');
